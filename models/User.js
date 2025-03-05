f93c11a14ac9bb69ab35c0bc3ede827b307247bc
Pushkal-vashishtha <pushkalvas730@gmail.com>
2025-03-05 10:59:15 +0530

    updates

--- models/User.js
+++ models/User.js
@@ -4,7 +4,6 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  phone: { type: String } // ✅ Sparse prevents null duplicates
 }, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
