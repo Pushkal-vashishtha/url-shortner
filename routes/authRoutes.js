0cf1b46846373e187e805ca5760ad891d57aa661
Pushkal Vashishtha <96442318+Pushkal-vashishtha@users.noreply.github.com>
2025-03-07 00:01:42 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js"; // ✅ Import controller functions
+import { register, login } from "../controllers/authController.js"; // 
 
 const router = express.Router();
 
